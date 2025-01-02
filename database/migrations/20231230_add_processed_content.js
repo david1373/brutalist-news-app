35f2ef6db2d1af33c00ef8fa5e07cd40c7278262
WDevs <127317845+david1373@users.noreply.github.com>
2025-01-02 12:03:53 -0500

    Update migration to handle existing table

--- database/migrations/20231230_add_processed_content.js
+++ database/migrations/20231230_add_processed_content.js
@@ -1,13 +1,15 @@
 export function up(knex) {
   return knex.schema.table('articles', table => {
-    table.text('processed_content');
-    table.timestamp('processed_at');
+    table.text('processed_content').nullable();
+    table.timestamp('processed_at').nullable();
   });
 }
 
 export function down(knex) {
-  return knex.schema.table('articles', table => {
-    table.dropColumn('processed_content');
-    table.dropColumn('processed_at');
-  });
+  return Promise.all([
+    knex.schema.table('articles', table => {
+      table.dropColumn('processed_content');
+      table.dropColumn('processed_at');
+    })
+  ]);
 }
