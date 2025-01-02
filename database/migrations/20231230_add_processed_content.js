d9a83a7069871d4e34b9cb0a072930e0bff7e715
WDevs <127317845+david1373@users.noreply.github.com>
2025-01-02 12:02:52 -0500

    Fix migration file to use ES modules

--- database/migrations/20231230_add_processed_content.js
+++ database/migrations/20231230_add_processed_content.js
@@ -1,13 +1,13 @@
-exports.up = function(knex) {
+export function up(knex) {
   return knex.schema.table('articles', table => {
     table.text('processed_content');
     table.timestamp('processed_at');
   });
-};
+}
 
-exports.down = function(knex) {
+export function down(knex) {
   return knex.schema.table('articles', table => {
     table.dropColumn('processed_content');
     table.dropColumn('processed_at');
   });
-};
+}
